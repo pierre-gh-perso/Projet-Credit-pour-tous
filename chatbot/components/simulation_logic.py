cbf7e2f7bed6afc8ed539718bbe26aafe077b9b8
Pierre-Alexandre <pierrealexandre.simonnet@gmail.com>
2025-07-24 10:32:03 +0200

    MAJ2 24.07.2025

--- chatbot/components/simulation_logic.py
+++ chatbot/components/simulation_logic.py
@@ -5,21 +5,17 @@ import os
 
 @st.cache_resource
 def load_simple_proxy_model():
-    """
-    Charge le modèle proxy SIMPLIFIÉ et sa structure de colonnes.
-    """
     base_path = os.path.join("..", "models")
     try:
         assets = {
-            "proxy_model": joblib.load(os.path.join(base_path, "chatbot_proxy_model_SIMPLE.pkl")),
-            "model_columns": joblib.load(os.path.join(base_path, "chatbot_proxy_model_SIMPLE_columns.pkl")),
+            "proxy_model": joblib.load(os.path.join(base_path, "chatbot_model.pkl")),
+            "model_columns": joblib.load(os.path.join(base_path, "chatbot_model_columns.pkl")),
         }
         return assets
     except FileNotFoundError as e:
-        st.error(f"Erreur : {e}. Entraînez le modèle proxy SIMPLIFIÉ.")
+        st.error(f"Erreur : {e}. Entraînez le modèle.")
         return None
 
-# Grille tarifaire basée sur l'analyse des données historiques
 RATE_GRID = {
     'A': {36: 6.99 / 100, 60: 7.55 / 100}, 'B': {36: 10.49 / 100, 60: 10.99 / 100},
     'C': {36: 13.66 / 100, 60: 14.27 / 100}, 'D': {36: 16.99 / 100, 60: 17.86 / 100},
@@ -28,7 +24,6 @@ RATE_GRID = {
 }
 
 def calculate_monthly_payment(principal, annual_rate_decimal, years=3):
-    """Calcule la mensualité d'un prêt."""
     monthly_rate = annual_rate_decimal / 12
     n_payments = int(years * 12)
     if monthly_rate <= 0: return principal / n_payments
@@ -47,9 +42,9 @@ def get_loan_proposal(form_data, assets):
     if risk_score < 0.14:   grade, decision = "A", "✅ Approbation"
     elif risk_score < 0.24: grade, decision = "B", "✅ Approbation"
     elif risk_score < 0.33: grade, decision = "C", "✅ Approbation"
-    elif risk_score < 0.41: grade, decision = "D", "⚠️ Analyse Requise"
-    elif risk_score < 0.50: grade, decision = "E", "⚠️ Analyse Requise"
-    else: grade, decision = "F+", "❌ Refus Prudent"
+    elif risk_score < 0.41: grade, decision = "D", "⚠️ Analyse requise"
+    elif risk_score < 0.50: grade, decision = "E", "⚠️ Analyse requise"
+    else: grade, decision = "F+", "❌ Analyse approfondie requise"
     
     rate = 0
     if "Refus" not in decision:
@@ -58,7 +53,6 @@ def get_loan_proposal(form_data, assets):
     # 3. Calculs financiers
     monthly_payment, total_repaid, credit_cost, final_dti = 0, 0, 0, form_data['dti']
     if rate > 0:
-        # On utilise la fonction calculate_monthly_payment
         monthly_payment = calculate_monthly_payment(form_data['loan_amnt'], rate, years=form_data['term'] / 12)
         total_repaid = monthly_payment * form_data['term']
         credit_cost = total_repaid - form_data['loan_amnt']
